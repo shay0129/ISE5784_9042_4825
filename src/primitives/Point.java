90da5818a09896c37fe982630a7df59c747b46ec
ShayMordechai <shaimordehai@gmail.com>
2024-05-21 14:55:11 +0300

    first update

--- src/primitives/Point.java
+++ src/primitives/Point.java
@@ -1,2 +1,49 @@
-package primitives;public class Point {
+package primitives;
+
+public class Point {
+
+    // Assuming a custom Double3 class exists to represent 3D coordinates
+    private final Double3 xyz;
+
+    // Ctor
+    public Point(double x, double y, double z) {
+        this.xyz = new Double3(x, y, z);
+    }
+    public Point(Double3 xyz){
+        this.xyz = xyz;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Point point = (Point) o;
+        return xyz.equals(point.xyz);
+    }
+
+    @Override
+    public int hashCode() {
+        return xyz.hashCode();
+    }
+
+    @Override
+    public String toString() {
+        return "Point (" + xyz.getX() + ", " + xyz.getY() + ", " + xyz.getZ() + ")";
+    }
+
+    public double distanceSquared(Point point) {
+        double dx = point.x - this.x;
+        double dy = point.y - this.y;
+        double dz = point.z - this.z;
+        return dx * dx + dy * dy + dz * dz;
+    }
+
+    public double distance(Point point) {
+        return Math.sqrt(distanceSquared(point));
+    }
+
+    public Point add(Vector vector) {
+        return new Point(x + vector.head.x, y + vector.head.y, z + vector.head.z);
+    }
 }
+
--- src/primitives/Vector.java
+++ src/primitives/Vector.java
@@ -1,2 +1,52 @@
-package primitives;public class Vector {
+package primitives;
+
+import static primitives.Util.alignZero;
+public class Vector extends Point{
+    public Vector(double x, double y, double z) {
+        super(x, y, z);
+        if (xyz.equals(Double3.ZERO))
+            throw new ArithmeticException("Vector(0,0,0) is not valid");
+    }
+
+    public Vactor(Double3 xyz){
+        this.xyz = xyz;
+        if (xyz.equals(Double3.ZERO))
+            throw new ArithmeticException("Vector(0,0,0) is not valid");
+    }
+
+    @Override
+    public String toString() {
+        return "Vector (" + xyz + ")";
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Vector vector)) return false;
+        return xyz.equals(vector.xyz);
+    }
+
+    public Vector add(Vector vector) {
+        return new Vector(xyz.add(vector.xyz));
+    }
+
+    public double scale(double scalar) {
+        return new Vector(xyz.d1 * scalar, xyz.d2 * scalar, xyz.d3 * scalar);
+    }
+
+    public double lengthSquared() {
+        return xyz.d1xyz.d1 + xyz.d2 * xyz.d2 + xyz.d3 * xyz.d3;
+    }
+
+    public double length() {
+        return Math.sqrt(lengthSquared());
+    }
+
+    public Vector normalize() {
+        double length = length();
+        if (length == 0) {
+            throw new ArithmeticException("Cannot normalize Zero Vector");
+        }
+        return new Vector(head.d1 / length, head.d2 / length, head.d3 / length);
+    }
 }
