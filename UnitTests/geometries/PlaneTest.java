292da5dbacce6b7062ccbd3c0a81fcbda2d09861
ShayMordechai <shaimordehai@gmail.com>
2024-06-07 15:34:29 +0300

    Friday 7/6/24 updated by Shay

--- UnitTests/geometries/PlaneTest.java
+++ UnitTests/geometries/PlaneTest.java
@@ -9,7 +9,7 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class PlaneTests {
+class PlaneTest {
 
     /**
      * Test method for {@link geometries.Plane#getNormal(primitives.Point)}.
--- UnitTests/geometries/SphereTest.java
+++ UnitTests/geometries/SphereTest.java
@@ -11,7 +11,7 @@ import static org.junit.jupiter.api.Assertions.*;
 /**
  * @author Dan Zilberstein
  */
-class SphereTests {
+class SphereTest {
 
     /**
      * Test method for {@link geometries.Sphere#getNormal(primitives.Point)}.
